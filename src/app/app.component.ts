7dec2a92012f29b722ae1cf3a7cdb6fe50824020
Vtzinn0 <adrielviturino004@gmail.com>
2024-11-14 21:30:10 -0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContatoComponent } from "./pages/contato/contato.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContatoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
